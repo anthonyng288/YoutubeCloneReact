8516784494eb834f03263a550eaf40365d90fca5
anthonyng288 <anthonynguyen832@gmail.com>
2024-11-28 16:53:15 +1000

    Add and modify tests
    Signed-off-by: anthonyng288 <anthonynguyen832@gmail.com>

--- src/components/Navbar.test.tsx
+++ src/components/Navbar.test.tsx
@@ -1,14 +1,29 @@
 import { render, screen } from "@testing-library/react";
 import Navbar from "./Navbar";
 
-vi.mock("../utils/components", () => ({
-  logo: "test-logo.png",
-}));
-
 describe("Navbar component", () => {
-  test("Renders the nav bar successfully", () => {
+  beforeEach(() => {
     render(<Navbar />);
+  });
+
+  test("Renders the nav bar successfully", () => {
     const navbarElement = screen.getByTestId("navbar");
     expect(navbarElement).toBeInTheDocument();
+  });
+
+  test("Renders the logo with correct attributes", () => {
+    const logoElement = screen.getByAltText("logo");
+    expect(logoElement).toBeInTheDocument();
+    // Logo URL from constants.ts
+    expect(logoElement).toHaveAttribute(
+      "src",
+      "https://i.ibb.co/s9Qys2j/logo.png"
+    );
+    expect(logoElement).toHaveAttribute("height", "45");
+  });
+
+  test("Contains a link that navigates to the home page", () => {
+    const linkElement = screen.getByRole("link", { name: /logo/i });
+    expect(linkElement).toHaveAttribute("href", "/");
   });
 });
